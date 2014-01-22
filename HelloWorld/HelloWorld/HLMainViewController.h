d0240e226fe69c2f26d1c5c3ec5f2e756b9a3343
Vishwesh K Joshi <vishwesh.joshi@robosoftin.com>
2014-01-22 14:25:49 +0530

    Second Commit

---
+++ HelloWorld/HelloWorld/HLMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  HLMainViewController.h
+//  HelloWorld
+//
+//  Created by Vishwesh K Joshi on 22/01/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLMainViewController : UIViewController
+
+@end
---
+++ HelloWorld/HelloWorld/HLMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  HLMainViewController.m
+//  HelloWorld
+//
+//  Created by Vishwesh K Joshi on 22/01/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import "HLMainViewController.h"
+
+@interface HLMainViewController ()
+
+@end
+
+@implementation HLMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
