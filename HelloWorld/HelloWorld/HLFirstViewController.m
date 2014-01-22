0b20e7e7e06ce50f04411ccd6f9bb55459d307fc
Vishwesh K Joshi <vishwesh.joshi@robosoftin.com>
2014-01-22 14:30:18 +0530

    Second Commit

---
+++ HelloWorld/HelloWorld/HLFirstViewController.m
@@ -0,0 +1,38 @@
+//
+//  HLFirstViewController.m
+//  HelloWorld
+//
+//  Created by Vishwesh K Joshi on 22/01/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import "HLFirstViewController.h"
+
+@interface HLFirstViewController ()
+
+@end
+
+@implementation HLFirstViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
