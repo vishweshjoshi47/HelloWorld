5e08d582808e77e1fc5b2f15377c7a42661adc26
Vishwesh K Joshi <vishwesh.joshi@robosoftin.com>
2014-01-22 14:14:49 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorld/HLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HLAppDelegate.h
+//  HelloWorld
+//
+//  Created by Vishwesh K Joshi on 22/01/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Vishwesh K Joshi on 22/01/14.
+//  Copyright (c) 2014 Vishwesh K Joshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HLAppDelegate class]));
+    }
+}
